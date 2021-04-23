129ae97259274f3b877430f9f32362a265cb58b2
group6BCS1 <charlottehelga8@gmail.com>
2021-04-23 04:38:25 -0800

    number of denominations to be given

--- src/chapter5/exercise3.py
+++ src/chapter5/exercise3.py
@@ -6,7 +6,7 @@ ones = 0
 fives = 0
 
 x = ('Menu for deposits :\nq-deposit a quarters\nd-deposit a dimes\n'
-     'n-deposit a nickels\no-deposit a ones \nf-deposit a fives\n'
+     'n-deposit a nickels\no-deposit a one dollar bill \nf-deposit a five dollar bill\n'
      .format(quarters, dimes, nickels, ones, fives))
 
 n = "nickel"
@@ -23,9 +23,10 @@ print('stock contains :\n{} quarters\n{} dimes\n{} nickels\n{} ones \n{} fives\n
 while True:
     int_str = input("\nPlease enter purchase price (XX.XX) or 'q' to quit:")
     if int_str == 'q':
-        break
+        break  # this terminates the program the moment q is entered
     price = float(int_str)
-    if price > 0 and (price % 5 * 100) != 0:
+    if price > 0 or (price % 5 * 100) != 0:  # this makes sure that the purchase price is is a non negative value or a
+        # multiple of 5
         print(x)
         break
     else:
@@ -37,7 +38,7 @@ while int_str != 'q':
         if dollar > price:
             price = price * 100
             dollar *= 100
-            print()
+            print(x)
             break
         else:
             print('insufficient amount')
@@ -48,7 +49,7 @@ while int_str != 'q':
     noOfOnes = 0
     noOfFives = 0
     change = dollar - price
-    print('your change is $', (change / 100))
+    print('Please take the change below \n', (change / 100))
 
     noOfQuarters = int(change / 25)
     if noOfQuarters != 0:
@@ -80,3 +81,29 @@ while int_str != 'q':
         noOfNickels = fives
         fives = 0
         change -= noOfNickels * 5
+
+    totalCoin = noOfDimes + noOfQuarters + noOfNickels + noOfOnes + noOfFives
+    print('\n Least number of coin needed to change is {}'.format(int(totalCoin)))
+    if noOfQuarters != 0:
+        print('{} quarters'.format(noOfQuarters))
+    if noOfDimes != 0:
+        print('{} dimes'.format(noOfDimes))
+    if noOfNickels != 0:
+        print('{} nickels'.format(noOfNickels))
+    if noOfOnes != 0:
+        print('{} ones'.format(noOfOnes))
+    if noOfFives != 0:
+        print('{} fives'.format(noOfFives))
+
+    print('\nStock contains : {}quarters\n{}dimes\n {}nickels\n {}ones\n and {}fives'
+          .format(quarters, dimes, nickels, ones, fives))
+    while True:
+        int_str = input('\n\n\nPlease enter the price in for of XX.XX or q to quit:')
+        if int_str == 'q':
+            break
+        price = float(int_str)
+        if price > 0 or (price % 5 * 100) != 0:  # this makes sure that the purchase price is is a non negative value
+            # or a multiple of 5
+            break
+        else:
+            print('Illegal price: must be  a non negative multiple of five cents')
