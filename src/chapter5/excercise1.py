59b973854f5cf98338462f69d41bf40d9bb4a57e
group6BCS1 <charlottehelga8@gmail.com>
2021-04-24 08:29:24 -0800

    chapter5

--- src/chapter5/excercise1.py
+++ src/chapter5/excercise1.py
@@ -1,11 +1,14 @@
 try:
     total = 0
     count = 0
+# the user will be able to input numbers repeatedly
     while True:
         x = input('enter a number')
+# when done is entered, the loop will be broken
         if x == 'done':
             break
         x = int(x)
+# we are finding the total, count and average of the numbers entered
         total = total + x
         count = count + 1
         average = total / count
