a54a5fbec615a3bcf8d51e2b531958674ab3c1bb
group17BSE1 <jonathanmarksemiyagi@gmail.com>
2021-05-20 06:03:29 +0100

    project2_a populating output file

--- src/project2/project2_a.py
+++ src/project2/project2_a.py
@@ -1,20 +1,23 @@
 # The program in “project2_a.py” will copy selected lines from “measles.txt” into a
 # file selected by the user.
 try:
-    file1 = open('measles.txt', 'r')
+    file_input = open('measles.txt', 'r')
 except FileNotFoundError:
     print('File cannot be opened:', ' measles.txt')
     quit()
-while True:
-    out_put_file = input('enter output file >>>')
-    year = input('enter the year')
-    if len(year) == 4:
-        converted_year = year
-        output_file = open('file_out', 'w')
-        for line in 'file1':
-            if  converted_year == line[88:88 + len(year)]:
-                output_file.write(line)
-            elif year == ("", "all", "ALL"):
-                output_file.write(line)
-    else:
-        print('enter correct year')  # this pops up when  the length of the year is not 4
+
+out_put_file = input('enter output file >>>')
+year = (input('enter the year>>>'))
+converted_year = year
+if len(year) <=4:
+    out_put_file = open(out_put_file, 'w')
+if len(year) >= 5:
+    print('Please enter correct year')  # this pops up when  the length of the year is not 4
+for line in file_input:
+    if converted_year == line[88:88 + len(year)]:
+        out_put_file.write(line)
+    elif year == ("", "all", "ALL"):
+        out_put_file.write(line)
+
+file_input.close()
+out_put_file.close()
